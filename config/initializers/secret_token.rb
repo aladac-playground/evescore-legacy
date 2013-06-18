6bf98da4b9174500bccf806d06b9859b880a18d3
Adam Ladachowski <chi@sazabi.pl>
2013-06-18 11:15:21 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evescore::Application.config.secret_token = '11bb161694659200b759b47d3503278262d5be8dc59e324b036c75b139337b5c4dd63f5dfdad33d2317eb279f528c0b2e47888bfd26c47050029607ba67015e5'
