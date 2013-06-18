544784d887f712239f655a4d0bcd86ae13458f22
Adam Ladachowski <chi@sazabi.pl>
2013-06-18 18:26:33 +0200

    - changed the selector for alerts to .notification-area

--- app/assets/javascripts/key.js
+++ app/assets/javascripts/key.js
@@ -10,12 +10,12 @@ function check_api (key, vcode) {
         ).done(function ( data ) {
           result = JSON.parse(data);
           if ( result['valid'] == false ) {
-            $('#noti-area').html('<div class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button>' + result['message'] + '</div>');
+            $('.notification-area').html('<div class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button>' + result['message'] + '</div>');
           } else if ( result['valid'] == true ) {
             character = result['character'];
             $('#api_submit').show();
             $('#api_check').hide();
-            $('#noti-area').html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Success</div>');
+            $('.notification-area').html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Success</div>');
             $('#key_id').attr('disabled','true');
             $('#vcode').attr('disabled','true');
             $('#portrait').html('<img src="http://image.eveonline.com/Character/' + character['char_id'] + '_128.jpg">');
@@ -23,7 +23,7 @@ function check_api (key, vcode) {
           }
         });
   } else {
-    $('#noti-area').html('<div class="alert alert-warning"><button type="button" class="close" data-dismiss="alert">&times;</button>Check the format</div>');
+    $('.notification-area').html('<div class="alert alert-warning"><button type="button" class="close" data-dismiss="alert">&times;</button>Check the format</div>');
   }
 };
 
