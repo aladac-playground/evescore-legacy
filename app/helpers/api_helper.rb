b9dd4190142f6f6d166201c0de4943e5f96e93d2
Adam Ladachowski <chi@sazabi.pl>
2013-06-18 11:28:47 +0200

    - added API wallet access checking helper

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -1,2 +1,22 @@
 module ApiHelper
+  def api_check(key, vcode)
+    uri = URI.parse "https://api.eveonline.com/char/WalletJournal.xml.aspx?keyID=#{key_id}&vcode=#{vcode}&rowCount=1"
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    response = http.request(request)
+
+    xml = Nokogiri::XML(response.body)
+
+    error_message = xml.xpath("//error").children.to_s
+    if error_message.empty?
+      return true
+    else
+      flash[:error] = error_message
+      return false
+    end
+  end
 end
