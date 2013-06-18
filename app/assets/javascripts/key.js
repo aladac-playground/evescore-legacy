4f078ecb9d8e5d834e1831e5786a40dda8875233
Adam Ladachowski <chi@sazabi.pl>
2013-06-18 19:51:11 +0200

    - changed disabled attribute to readonly as disabled prevended sending   the form data

--- app/assets/javascripts/key.js
+++ app/assets/javascripts/key.js
@@ -16,10 +16,10 @@ function check_api (key, vcode) {
             $('#api_submit').show();
             $('#api_check').hide();
             $('.notification-area').html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Success</div>');
-            $('#key_id').attr('disabled','true');
-            $('#vcode').attr('disabled','true');
-            $('#portrait').html('<img src="http://image.eveonline.com/Character/' + character['char_id'] + '_128.jpg">');
-            $('#name').html(character['name']);
+            $('#key_id').attr('readonly','true');
+            $('#vcode').attr('readonly','true');
+            $('#name').val(character['name']);
+            $('#char_id').val(character['char_id']);
           }
         });
   } else {
