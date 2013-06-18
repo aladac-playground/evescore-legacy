0d66d4a387c3372d40077317c6ccddc1ddb99fc5
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-06-18 22:24:38 +0000

    started preparing a sidekiq job to get wallet data

--- app/workers/get_wallet_data.rb
+++ app/workers/get_wallet_data.rb
@@ -2,6 +2,19 @@ class GetWalletData
   include Sidekiq::Worker
 
   def perform( name, count )
-    puts "Hello!"
+    Character.all.each do |character|
+      uri = URI.parse "https://api.eveonline.com/char/WalletJournal.xml.aspx?keyID=#{key}&vcode=#{vcode}&rowCount=1"
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+
+      response = http.request(request)
+
+      xml = Nokogiri::XML(response.body)
+
+      error_message = xml.xpath("//error").children.to_s
+    end
   end
 end
