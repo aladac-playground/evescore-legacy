4367ec8d75eea401bea40574bc15c2b70c306eab
Adam Ladachowski <chi@sazabi.pl>
2013-06-18 17:58:23 +0200

    - added HTML form validations - added length validation of vcode and key before passed to AJAX

--- app/assets/javascripts/key.js
+++ app/assets/javascripts/key.js
@@ -1,26 +1,30 @@
 function check_api (key, vcode) {
-  $.ajax(
-    {
-      url: "/api/verify",
+  if ( key.length == 7 && vcode.length == 64 ) {
+    $.ajax(
+        {
+          url: "/api/verify",
       dataType: "HTML",
       data: { key: key, vcode: vcode },
       processData: true
-    }
-  ).done(function ( data ) {
-    result = JSON.parse(data);
-    if ( result['valid'] == false ) {
-      $('#noti-area').html('<div class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button>' + result['message'] + '</div>');
-    } else if ( result['valid'] == true ) {
-      character = result['character'];
-      $('#api_submit').show();
-      $('#api_check').hide();
-      $('#noti-area').html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Success</div>');
-      $('#key_id').attr('disabled','true');
-      $('#vcode').attr('disabled','true');
-      $('#portrait').html('<img src="http://image.eveonline.com/Character/' + character['char_id'] + '_128.jpg">');
-      $('#name').html(character['name']);
-    }
-  });
+        }
+        ).done(function ( data ) {
+          result = JSON.parse(data);
+          if ( result['valid'] == false ) {
+            $('#noti-area').html('<div class="alert"><button type="button" class="close" data-dismiss="alert">&times;</button>' + result['message'] + '</div>');
+          } else if ( result['valid'] == true ) {
+            character = result['character'];
+            $('#api_submit').show();
+            $('#api_check').hide();
+            $('#noti-area').html('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">&times;</button>Success</div>');
+            $('#key_id').attr('disabled','true');
+            $('#vcode').attr('disabled','true');
+            $('#portrait').html('<img src="http://image.eveonline.com/Character/' + character['char_id'] + '_128.jpg">');
+            $('#name').html(character['name']);
+          }
+        });
+  } else {
+    $('#noti-area').html('<div class="alert alert-warning"><button type="button" class="close" data-dismiss="alert">&times;</button>Check the format</div>');
+  }
 };
 
 $(document).ready(function(){
