45ad97fce8ca261da6f0bd78f1c7205bfd914b10
Adam Ladachowski <chi@sazabi.pl>
2013-06-18 17:40:43 +0200

    Fixed the character validations

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character
   include Mongoid::Document
-  validates :key, :presence => true, :numericality => true, :length => 7
-  validates :vcode, :presence => true, :length => 64
+  validates :key, :presence => true, :numericality => true, :length => { :is => 7 }
+  validates :vcode, :presence => true, :length => { :is => 64 }
   field :char_id, type: Integer
   field :name, type: String
   field :key, type: String
