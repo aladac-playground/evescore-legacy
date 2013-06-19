6eb29faee47f874d51d72a5de3e8f990aaf960bd
Adam Ladachowski <chi@sazabi.pl>
2013-06-19 14:16:00 +0200

    - added helpers to display images from image.eveonline.com

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     character = Character.where(char_id: id).first
     return character[:name]
   end
+  def rat_name(id)
+    rat = Rat.where(rat_id: id).first
+    return rat[:rat_name]
+  end
+  def rat_image(id)
+    return "http://image.eveonline.com/Type/#{id}_32.png"
+  end
+  def character_image(id)
+    return "http://image.eveonline.com/Character/#{id}_32.jpg"
+  end
 end
