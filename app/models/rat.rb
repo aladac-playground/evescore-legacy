3bd748d26fd97437bb4781cad2ff864e01633cd7
Adam Ladachowski <chi@sazabi.pl>
2013-06-19 14:03:56 +0200

    Added rat model + index + validations

---
+++ app/models/rat.rb
@@ -0,0 +1,8 @@
+class Rat
+  include Mongoid::Document
+  validates :rat_id, :uniqueness => true
+  field :rat_id, type: Integer
+  field :rat_name, type: String
+  field :rat_type, type: String
+  index({ rat_id: 1 }, { unique: true })
+end
