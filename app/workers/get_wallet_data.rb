578913d023232ccd0c8c927db3d117bcb323e3a7
Adam Ladachowski <chi@sazabi.pl>
2013-06-19 10:47:43 +0200

    Further work on the import job, not finished - need kill, bounty models

--- app/workers/get_wallet_data.rb
+++ app/workers/get_wallet_data.rb
@@ -1,9 +1,12 @@
 class GetWalletData
   include Sidekiq::Worker
 
-  def perform( name, count )
+  def perform(name, count)
     Character.all.each do |character|
-      uri = URI.parse "https://api.eveonline.com/char/WalletJournal.xml.aspx?keyID=#{key}&vcode=#{vcode}&rowCount=1"
+      puts "Getting data for: #{character[:name]}"
+      vcode = character[:vcode]
+      key_id = character[:key]
+      uri = URI.parse "https://api.eveonline.com/char/WalletJournal.xml.aspx?keyID=#{key_id}&vcode=#{vcode}&rowCount=1000"
       http = Net::HTTP.new(uri.host, uri.port)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
@@ -14,7 +17,35 @@ class GetWalletData
 
       xml = Nokogiri::XML(response.body)
 
-      error_message = xml.xpath("//error").children.to_s
+      xml.xpath("//row").each do |row|
+        if row['refTypeID'] == "85"
+          reason = row['reason']
+          amount = row['amount'].to_i
+          owner_name = row['ownerName2']
+          owner_id = row['ownerID2']
+          ref_id = row['refID']
+          ts = row['date']
+          begin
+            DB[:bounty].insert([:ts, :char_id, :bounty],[ts, owner_id, amount.to_f])
+          rescue Exception => e
+            if e.message !~ /Mysql2::Error: Duplicate entry/
+              p e.message
+            end
+          end
+          reason.split(",").each do |entry|
+            entry = entry.split(":")
+            rat_id = entry[0]
+            rat_amount = entry[1]
+            begin
+              DB[:kills].insert([:ts, :ref_id, :char_id, :rat_id, :rat_amount, :bounty], [ts, ref_id, owner_id, rat_id, rat_amount, amount])
+            rescue Exception => e
+              if e.message !~ /Mysql2::Error: Duplicate entry/
+                p e.message
+              end
+            end
+          end
+        end
+      end
     end
   end
 end
