bcfd59371f09ece222ca4c827cd2d1a277fb5e88
Adam Ladachowski <chi@sazabi.pl>
2013-06-20 14:10:53 +0200

    - create helpers to link character and rat name to images and names - used helpers in front page view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,29 @@ module ApplicationHelper
     character = Character.where(char_id: id).first
     return truncate character[:name], :length => len
   end
+  def character_name_link(id, len=15)
+    character = Character.where(char_id: id).first
+    link_to truncate(character[:name], :length => len), kills_log_path(:filter => { :char_id => id } )
+  end
   def rat_name(id, len=18)
     rat = Rat.where(rat_id: id).first
     return truncate rat[:rat_name], :length => len
   end
+  def rat_name_link(id, len=18)
+    rat = Rat.where(rat_id: id).first
+    link_to truncate(rat[:rat_name], :length => len), kills_log_path(:filter => { :rat_id => id } )
+  end
   def rat_image(id, size=64)
     return "http://image.eveonline.com/Type/#{id}_#{size}.png"
   end
+  def rat_image_link(id, size=64)
+    link_to image_tag("http://image.eveonline.com/Type/#{id}_#{size}.png", :class => "img-rounded"), kills_log_path(:filter => { :rat_id => id } )
+  end
   def character_image(id, size=64)
     return "http://image.eveonline.com/Character/#{id}_#{size}.jpg"
+  end
+  def character_image_link(id, size=64)
+    link_to image_tag("http://image.eveonline.com/Character/#{id}_#{size}.jpg", :class => "img-rounded"), kills_log_path(:filter => { :char_id => id } )
   end
   def top_bounty(limit=4)
     top = Bounty.collection.aggregate( { "$group" => { "_id" => "$char_id", "sum" => { "$sum" => "$bounty"}  } }, {"$sort" => { "sum" => -1 } } )[0..limit]
