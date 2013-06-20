f49f296e002c70ea9d12cc5f40c4cb2cacadd0cb
Adam Ladachowski <chi@sazabi.pl>
2013-06-20 11:58:32 +0200

    - added size argument to rat_image - added helpers to generate 10 days data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,11 +7,11 @@ module ApplicationHelper
     rat = Rat.where(rat_id: id).first
     return rat[:rat_name]
   end
-  def rat_image(id)
-    return "http://image.eveonline.com/Type/#{id}_32.png"
+  def rat_image(id, size=64)
+    return "http://image.eveonline.com/Type/#{id}_#{size}.png"
   end
-  def character_image(id)
-    return "http://image.eveonline.com/Character/#{id}_32.jpg"
+  def character_image(id, size=64)
+    return "http://image.eveonline.com/Character/#{id}_#{size}.jpg"
   end
   def top_bounty(limit=4)
     top = Bounty.collection.aggregate( { "$group" => { "_id" => "$char_id", "sum" => { "$sum" => "$bounty"}  } }, {"$sort" => { "sum" => -1 } } )[0..limit]
@@ -21,10 +21,23 @@ module ApplicationHelper
     bounty = Rat.where(rat_id: id).first[:bounty]
     return bounty
   end
+  def top_kills_10days(limit=4)
+    top = Kill.
+      collection.aggregate(  { "$match" => { "ts" => { "$gt" => (Time.now.utc - 10.days) } } }, { "$group" => { "_id" => "$char_id", "sum" => { "$sum" => "$rat_amount"}  } }, {"$sort" => { "sum" => -1 } } )[0..limit]
+    return top
+  end
+  def top_bounty_10days(limit=4)
+    top = Bounty.
+      collection.aggregate(  { "$match" => { "ts" => { "$gt" => (Time.now.utc - 10.days) } } }, { "$group" => { "_id" => "$char_id", "sum" => { "$sum" => "$bounty"}  } }, {"$sort" => { "sum" => -1 } } )[0..limit]
+    return top
+  end
+  def highest_bounty_10days(limit=5)
+    bounty = Kill.where(:ts.gt => (Time.now.utc - 10.days)).sort(:bounty.desc).limit(limit)
+    return bounty
+  end
   def top_monthly(limit=4)
     top = Bounty.
-      where(:ts.gt => Time.now.utc.at_beginning_of_month.utc ).
-      collection.aggregate( { "$group" => { "_id" => "$char_id", "sum" => { "$sum" => "$bounty"}  } }, {"$sort" => { "sum" => -1 } } )[0..limit]
+      collection.aggregate(  { "$match" => { "ts" => { "$gt" => Time.now.utc.at_beginning_of_month.utc } } }, { "$group" => { "_id" => "$char_id", "sum" => { "$sum" => "$bounty"}  } }, {"$sort" => { "sum" => -1 } } )[0..limit]
     return top
   end
   def isk(amount)
