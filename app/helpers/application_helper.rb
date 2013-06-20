7f1ff6e853cf7dafeaea771c397a491a91414703
Adam Ladachowski <chi@sazabi.pl>
2013-06-20 13:06:25 +0200

    - added highest tick helper - added truncate length for show name helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-  def character_name(id)
+  def character_name(id, len=15)
     character = Character.where(char_id: id).first
-    return character[:name]
+    return truncate character[:name], :length => len
   end
-  def rat_name(id)
+  def rat_name(id, len=18)
     rat = Rat.where(rat_id: id).first
-    return rat[:rat_name]
+    return truncate rat[:rat_name], :length => len
   end
   def rat_image(id, size=64)
     return "http://image.eveonline.com/Type/#{id}_#{size}.png"
@@ -35,10 +35,24 @@ module ApplicationHelper
     bounty = Kill.where(:ts.gt => (Time.now.utc - 10.days)).sort(:bounty.desc).limit(limit)
     return bounty
   end
+  def highest_bounty(limit=5)
+    bounty = Kill.all.sort(:bounty.desc).limit(limit)
+    return bounty
+  end
+  def highest_tick_10days(limit=5)
+    bounty = Bounty.where(:ts.gt => (Time.now.utc - 10.days)).sort(:bounty.desc).limit(limit)
+    return bounty
+  end
   def top_monthly(limit=4)
     top = Bounty.
       collection.aggregate(  { "$match" => { "ts" => { "$gt" => Time.now.utc.at_beginning_of_month.utc } } }, { "$group" => { "_id" => "$char_id", "sum" => { "$sum" => "$bounty"}  } }, {"$sort" => { "sum" => -1 } } )[0..limit]
     return top
+  end
+  def isk_short(amount)
+    isk = number_to_human(amount)
+    isk.gsub!(/ Million/,"M ISK")
+    isk.gsub!(/ Billion/,"b ISK")
+    return isk
   end
   def isk(amount)
     isk = number_to_currency(amount, :unit => "ISK", :precision => 0, :delimiter => ",", :format => "%n %u")
