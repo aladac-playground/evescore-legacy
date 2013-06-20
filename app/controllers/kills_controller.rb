5acb60b3a50684b725928061c3cc7ac9ea76add5
Adam Ladachowski <chi@sazabi.pl>
2013-06-20 13:48:49 +0200

    added filtering of results based on filter param

--- app/controllers/kills_controller.rb
+++ app/controllers/kills_controller.rb
@@ -1,5 +1,15 @@
 class KillsController < ApplicationController
   def log
-    @kill_log = Kill.order_by(ts: 'desc').paginate(:page => params[:page], :per_page => 8)
+    @kill_log = Kill.all
+    if params[:filter]
+      params[:filter].each_pair do |fil, val|
+        fil = fil.to_s
+        p fil
+        if Kill.attribute_names.include?(fil)
+          @kill_log = @kill_log.where(fil.to_sym => val)
+        end
+      end
+    end
+    @kill_log = @kill_log.order_by(ts: 'desc').paginate(:page => params[:page], :per_page => 8)
   end
 end
