532187ddd755828d64197f99d5d78755d1888b18
Adam Ladachowski <chi@sazabi.pl>
2013-06-24 21:19:15 +0200

    Added a daily method to replace the application helper

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -4,4 +4,19 @@ class Bounty
   field :char_id, type: Integer
   field :bounty, type: Integer
   index({ ts: 1, char_id: 1 }, { unique: true })
+
+  def self.daily(char_id)
+    collection.aggregate( 
+                         { "$match" => { "char_id" => char_id } }, 
+                         { "$group" => { "_id" => 
+                           { 
+                             "char_id" => "$char_id", 
+                             "year" => { "$year" => "$ts" }, 
+                             "month" => { "$month" => "$ts" }, 
+                             "day" => { "$dayOfMonth" => "$ts" } 
+                           }, 
+                             "sum" => { "$sum" => "$bounty"} } }, 
+                             {"$sort" => { "_id" => -1 } }  
+                        )
+  end
 end
