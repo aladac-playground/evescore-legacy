841a7ed275388fdf0f45d8de90d08f9b2d7a7274
Adam Ladachowski <chi@sazabi.pl>
2013-06-25 00:32:49 +0200

    Changed date format to more human, added debug

--- app/helpers/character_helper.rb
+++ app/helpers/character_helper.rb
@@ -1,6 +1,13 @@
 module CharacterHelper
-  def build_date(id)
-    date = id["year"].to_s + "-" + id["month"].to_s + "-" + id["day"].to_s
+  def build_date(day)
+    year = day["_id"]["year"].to_i
+    month = day["_id"]["month"].to_i
+    day = day["_id"]["day"].to_i
+    p year.class
+    p month.class
+    p day.class
+    ts = Time.mktime(year, month, day)
+    date = ts.strftime("%b-%-d")
     return date
   end
 end
