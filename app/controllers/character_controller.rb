99e7a3a847b0a8625b20356ab9fc692251b5424c
Adam Ladachowski <chi@sazabi.pl>
2013-06-25 15:04:09 +0200

    - added checking for params[:char_id] - added instance variable @char_id to use in view

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,5 +1,10 @@
 class CharacterController < ApplicationController
   def profile
+    if ! params[:char_id]
+      redirect_to root_path
+    else
+      @char_id = params[:char_id]
+    end
     @daily = Bounty.daily(params[:char_id].to_i, 30)
   end
 end
