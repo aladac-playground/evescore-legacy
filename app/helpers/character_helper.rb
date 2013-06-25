8ae72ce1a9dff2553b22e84e762d28c4045d5729
Adam Ladachowski <chi@sazabi.pl>
2013-06-25 22:02:02 +0200

    disabled debug

--- app/helpers/character_helper.rb
+++ app/helpers/character_helper.rb
@@ -3,9 +3,6 @@ module CharacterHelper
     year = day["_id"]["year"].to_i
     month = day["_id"]["month"].to_i
     day = day["_id"]["day"].to_i
-    p year.class
-    p month.class
-    p day.class
     ts = Time.mktime(year, month, day)
     date = ts.strftime("%b-%-d")
     return date
