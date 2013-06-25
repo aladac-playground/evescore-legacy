2a17092851f23636c26abccb691b48a47dd770ba
Adam Ladachowski <chi@sazabi.pl>
2013-06-25 10:20:30 +0200

    removed validation

--- app/models/kill.rb
+++ app/models/kill.rb
@@ -3,6 +3,7 @@ class Kill
   field :ts, type: Time
   field :char_id, type: Integer
   field :rat_id, type: Integer
+  field :bounty, type: Integer
   field :rat_amount, type: Integer
   index({ ts: 1, char_id: 1, rat_id: 1 }, { unique: true })
 end
