19e0cf6a68774e625a75866135c3133e4a029647
Adam Ladachowski <chi@sazabi.pl>
2013-06-26 00:54:05 +0200

    Changed the old Kills to Bounty model

--- app/controllers/kills_controller.rb
+++ app/controllers/kills_controller.rb
@@ -1,15 +1,14 @@
 class KillsController < ApplicationController
   def log
-    @kill_log = Kill.all
+    @kill_log = Bounty.all
     if params[:filter]
       params[:filter].each_pair do |fil, val|
         fil = fil.to_s
-        p fil
-        if Kill.attribute_names.include?(fil)
+        if Bounty.attribute_names.include?(fil)
           @kill_log = @kill_log.where(fil.to_sym => val)
         end
       end
     end
-    @kill_log = @kill_log.order_by(ts: 'desc').paginate(:page => params[:page], :per_page => 12)
+    @kill_log = @kill_log.order_by(ts: 'desc').paginate(:page => params[:page], :per_page => 8)
   end
 end
