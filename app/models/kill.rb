2e5fede1d1fdf928ce7d423cca913f374f2f0aa0
Adam Ladachowski <chi@sazabi.pl>
2013-06-25 22:03:41 +0200

    - removed custom _id generation - removed extra fields - embedded in bounty

--- app/models/kill.rb
+++ app/models/kill.rb
@@ -1,19 +1,8 @@
 class Kill
   include Mongoid::Document
-  before_validation :generate_id
-  validates_uniqueness_of :ts, :scope => [ :char_id, :rat_id ] 
-  validates_presence_of :ts, :char_id, :rat_id, :bounty, :rat_amount
-  field :_id, type: Integer
-  field :ts, type: Time
-  field :char_id, type: Integer
+  validates_presence_of :rat_id, :rat_amount
+  embedded_in :bounty
   field :rat_id, type: Integer
-  field :bounty, type: Integer
   field :rat_amount, type: Integer
-  index({ ts: 1, char_id: 1, rat_id: 1 }, { unique: true })
-
-  private
-  def generate_id
-    # self._id ||= "#{rat_id.to_s}:#{ts.to_s}"
-    self._id ||= rat_id + ts.to_i + char_id
-  end
+  index({ rat_id: 1 }, { unique: true, drop_dups: true })
 end
