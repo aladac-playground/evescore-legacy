190e1dd3385ec36f63cf77427e30f50932f60612
Adam Ladachowski <chi@sazabi.pl>
2013-06-25 22:02:39 +0200

    - removed custom _id generation - added kills as embedded documents - added drop_dups to indexing

--- app/models/bounty.rb
+++ app/models/bounty.rb
@@ -1,12 +1,12 @@
 class Bounty
   include Mongoid::Document
-  before_validation :generate_id
   validates_uniqueness_of :ts, :scope => [ :char_id ]
   validates_presence_of :ts, :char_id, :bounty
   field :ts, type: Time
   field :char_id, type: Integer
   field :bounty, type: Integer
-  index({ ts: 1, char_id: 1 }, { unique: true })
+  index({ ts: 1, char_id: 1 }, { unique: true, drop_dups: true })
+  embeds_many :kills
 
   def self.daily(char_id, limit=10)
     limit -= 1
@@ -22,10 +22,5 @@ class Bounty
                              "sum" => { "$sum" => "$bounty"} } }, 
                              {"$sort" => { "_id" => -1 } }  
                         )[0..limit]
-  end
-
-  private
-  def generate_id
-    self._id ||= ts.to_i + char_id
   end
 end
