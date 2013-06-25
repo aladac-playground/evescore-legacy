2c9d0e79c2389a2d082f3bbee140c8842c8d9b7e
Adam Ladachowski <chi@sazabi.pl>
2013-06-25 16:42:04 +0200

    Added validation of params[:page]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :page_validation
+
+  private 
+  def page_validation
+    if params[:page].numeric? == false 
+      params[:page] = 1
+    elsif params[:page].to_i <= 0
+      params[:page] = 1
+    end
+  end
+end
+class String
+  def numeric?
+    Float(self) != nil rescue false
+  end
 end
