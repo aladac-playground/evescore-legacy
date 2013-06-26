43d09e3d97acb4d41235e83135d14465f19effa1
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-06-26 13:19:04 +0200

    Addded validation flash messages

--- app/controllers/key_controller.rb
+++ app/controllers/key_controller.rb
@@ -2,7 +2,17 @@ class KeyController < ApplicationController
   def add
   end
   def save
-    Character.create!(char_id: params[:char_id], name: params[:name], key: params[:key], vcode: params[:vcode])
-    flash[:notice] = "Ok"
+    char = Character.new(char_id: params[:char_id], name: params[:name], key: params[:key], vcode: params[:vcode])
+    if char.valid?
+      char.save
+      flash[:notice] = "Character key successfuly stored"
+    else
+      error_alert = String.new
+      char.errors.messages.each_pair do |field, message|
+        error_alert += message[0]
+      end
+      flash[:error] = error_alert
+      redirect_to key_add_path
+    end
   end
 end
