ae5daf66eff2c69aa7832844deae1815d3cba019
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-06-26 11:46:43 +0200

    created a char_id validation method, moved validation to before_filter

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,12 +1,19 @@
 class CharacterController < ApplicationController
+  before_filter :validate_char_id
+  
   def profile
-    if ! params[:char_id]
-      redirect_to root_path
-    else
-      @char_id = params[:char_id]
-    end
-    @kill_log = Bounty.where(char_id: @char_id)
+    @character = Character.where(:char_id => params[:char_id]).first
+    @kill_log = Bounty.where(char_id: params[:char_id])
     @kill_log = @kill_log.order_by(ts: 'desc').paginate(:page => params[:page], :per_page => 5)
     @daily = Bounty.daily(params[:char_id].to_i, 30)
+  end
+  
+  private
+  def validate_char_id
+    if ! params[:char_id] or params[:char_id].numeric? == false 
+      redirect_to root_path
+    elsif Character.where(:char_id => params[:char_id]).first == nil
+      redirect_to root_path
+    end
   end
 end
