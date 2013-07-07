96fed9017ebb9633beead4d96270dd24790501a0
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-07-07 16:54:48 +0200

    used the wallet_import method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -4,35 +4,7 @@ class ApiController < ApplicationController
     key = char.key
     api = Eve::Api.new(key[:key_id], key[:vcode])    
     wallet = api.wallet_journal
-    wallet.each do |row|
-      if row['refTypeID'] == "85"
-        reason = row['reason']
-        amount = row['amount'].to_i
-        owner_name = row['ownerName2']
-        owner_id = row['ownerID2']
-        ref_id = row['refID']
-        ts = row['date']
-        begin
-          b = Bounty.new(ts: ts, char_id: owner_id, bounty: ( amount * 100 ).to_i)
-          r = b.save
-          next if r == false
-          DebugLog.info("Character: #{character[:name]}, ID: #{character[:char_id]}, Bounty: #{amount}")
-        rescue Exception => e
-          p e.message
-        end
-        reason.split(",").each do |entry|
-          entry = entry.split(":")
-          rat_id = entry[0]
-          next if rat_id == "..."
-          rat_amount = entry[1]
-          begin
-            b.kills.create!(rat_id: rat_id, rat_amount: rat_amount)
-          rescue Exception => e
-            p e.message
-          end
-        end
-      end
-    end
+    wallet.wallet_import
     redirect_to character_profile_path(:char_id => params[:char_id])
   end
   def verify
