c56bbb79d2a855d3745412d60269425b740b68ce
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-07-11 21:57:57 +0200

    Added more validation for key verification

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -8,15 +8,25 @@ class ApiController < ApplicationController
     redirect_to character_profile_path(:char_id => params[:char_id])
   end
   def verify
-    api = Eve::Api.new(params[:key], params[:vcode])
-    characters = api.characters
-    if characters
-      character = characters.first
-      valid = true
-    else
+    if Key.where(key_id: params[:key]).count > 0
       valid = false
+      message = "Key already exists in database!"
+    else
+      api = Eve::Api.new(params[:key], params[:vcode])
+      characters = api.characters
+      if characters
+        character = characters.first
+        if Character.where(char_id: character[:char_id]).empty?
+          valid = true 
+        else
+          valid = false
+          message = "Character already exists in database!"
+        end
+      else
+        valid = false
+      end
     end
-    @verify = { valid: valid, character: character }
+    @verify = { valid: valid, character: character, message: message ? message : nil }
     respond_to do |format|
       format.json { render json: @verify }
     end
