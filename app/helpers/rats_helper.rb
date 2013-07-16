563a9dfcbe5b5073ee20efaa661899daaa321533
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-07-16 22:56:29 +0200

    working rudimentory rat info

--- app/helpers/rats_helper.rb
+++ app/helpers/rats_helper.rb
@@ -1,2 +1,41 @@
 module RatsHelper
+  def missile_damage
+    if @rat.missile_dps
+      missile_damage = Hash.new
+      @rat.missile_dps.each_pair do |k, v|
+        next if k == :charge_name
+        missile_damage[k] = sprintf "%.2f", v
+      end
+    end
+    missile_damage
+  end
+  def gun_damage
+    if @rat.gun_dps
+      gun_damage = Hash.new
+      @rat.gun_dps.each_pair do |k, v|
+        gun_damage[k] = sprintf "%.2f", v
+      end
+    end
+    gun_damage
+  end
+  def total_damage
+    total_damage = Hash.new
+    @rat.gun_dps.each_pair do |k, v|
+      total_damage[k] = v
+    end
+    @rat.missile_dps.each_pair do |k, v|
+      next if k == :charge_name
+      total_damage[k] = sprintf "%.2f", total_damage[k] + @rat.missile_dps[k]
+    end
+    total_damage
+  end
+  def mobility
+    mobility = {
+      # number_to_currency(1234567890.50, unit: "&pound;", separator: ",", delimiter: "", format: "%n %u")
+      orbit: number_to_currency(@rat.mobility[:orbit], precision: 0, delimiter: ",", format: "%n %u", unit: "m"),
+      max: number_to_currency(@rat.mobility[:max], precision: 0, delimiter: ",", format: "%n %u", unit: "m/s"),
+      cruise: number_to_currency(@rat.mobility[:cruise], precision: 0, delimiter: ",", format: "%n %u", unit: "m/s"),
+      range: number_to_currency(@rat.mobility[:range], precision: 0, delimiter: ",", format: "%n %u", unit: "m")
+    }
+  end
 end
--- app/models/rat.rb
+++ app/models/rat.rb
@@ -22,32 +22,99 @@ class Rat
       armor: rat_attributes.where(name: "armorHP").first.value
     }
   end
+  def bounty
+    rat_attributes.where(name: "entityKillBounty").first.value
+  end
   def shield_res
+    thermal = rat_attributes.where(name: "shieldThermalDamageResonance").first 
+    thermal ? thermal = (1 - thermal.value ) * 100 : thermal = 0
+    kinetic = rat_attributes.where(name: "shieldKineticDamageResonance").first 
+    kinetic ? kinetic = (1 - kinetic.value ) * 100 : kinetic = 0
+    explosive = rat_attributes.where(name: "shieldExplosiveDamageResonance").first 
+    explosive ? explosive = (1 - explosive.value ) * 100 : explosive = 0
+    em = rat_attributes.where(name: "shieldEmDamageResonance").first 
+    em ? em = (1 - em.value ) * 100 : em = 0
     {
-      kinetic: (1 - rat_attributes.where(name: "shieldKineticDamageResonance").first.value) * 100,
-      thermal: (1 - rat_attributes.where(name: "shieldThermalDamageResonance").first.value) * 100,
-      explosive: (1 - rat_attributes.where(name: "shieldExplosiveDamageResonance").first.value) * 100,
-      em: (1 - rat_attributes.where(name: "shieldEmDamageResonance").first.value) * 100,
+      kinetic: kinetic,
+      thermal: thermal,
+      explosive: explosive,
+      em: em
     }
   end
   def armor_res
+    kinetic = rat_attributes.where(name: "armorKineticDamageResonance").first.value
+    kinetic ? kinetic = sprintf("%.0f", (1 - kinetic) * 100.0) : kinetic = 0
+    thermal = rat_attributes.where(name: "armorThermalDamageResonance").first.value
+    thermal ? thermal = sprintf("%.0f", (1 - thermal) * 100.0) : thermal = 0
+    em = rat_attributes.where(name: "armorEmDamageResonance").first.value
+    em ? em = sprintf("%.0f", (1 - em) * 100.0) : em = 0
+    explosive = rat_attributes.where(name: "armorExplosiveDamageResonance").first.value
+    explosive ? explosive = sprintf("%.0f", (1 - explosive) * 100.0) : explosive = 0
     {
-      kinetic: (1 - rat_attributes.where(name: "armorKineticDamageResonance").first.value) * 100,
-      thermal: (1 - rat_attributes.where(name: "armorThermalDamageResonance").first.value) * 100,
-      explosive: (1 - rat_attributes.where(name: "armorExplosiveDamageResonance").first.value) * 100,
-      em: (1 - rat_attributes.where(name: "armorEmDamageResonance").first.value) * 100,
+      kinetic: kinetic,
+      thermal: thermal,
+      explosive: explosive,
+      em: em
     }
   end
   def speed
     rat_attributes.where(name: "maxVelocity").first.value
   end
   def gun_dps
-    multi = rat_attributes.where(name: "damageMultiplier").first.value
+    multi = rat_attributes.where(name: "damageMultiplier").first
+    if multi
+      multi = multi.value 
+      rate = rat_attributes.where(name: "speed").first.value / 1000
+      kinetic = rat_attributes.where(name: "kineticDamage").first
+      thermal = rat_attributes.where(name: "thermalDamage").first
+      explosive = rat_attributes.where(name: "explosiveDamage").first
+      em = rat_attributes.where(name: "emDamage").first
+      {
+        kinetic: kinetic ? kinetic.value * multi / rate : 0,
+        thermal: thermal ? thermal.value * multi / rate : 0,
+        explosive: explosive ? explosive.value * multi / rate : 0,
+        em: em ? em.value * multi / rate : 0
+      }
+    else
+      {
+        kinetic: 0,
+        thermal: 0,
+        explosive: 0,
+        em: 0
+      }
+    end
+  end
+  def missile_dps
+    multi = rat_attributes.where(name: "missileDamageMultiplier").first
+    if multi
+      rate = rat_attributes.where(name: "missileLaunchDuration").first.value / 1000
+      multi = multi.value
+      missile = Charge.where(charge_id: rat_attributes.where(name: "entityMissileTypeID").first.value.to_i ).first
+      {
+        kinetic: missile.charge_attributes.where(name: "kineticDamage").first.value * multi / rate,
+        thermal: missile.charge_attributes.where(name: "thermalDamage").first.value * multi / rate,
+        explosive: missile.charge_attributes.where(name: "explosiveDamage").first.value * multi / rate,
+        em: missile.charge_attributes.where(name: "emDamage").first.value * multi / rate,
+        charge_name: missile.name
+      }
+    else
+      {
+        kinetic: 0,
+        thermal: 0,
+        explosive: 0,
+        em: 0
+      }
+    end
+  end
+  def mobility
+    cruise = rat_attributes.where(name: "entityCruiseSpeed").first 
+    max = rat_attributes.where(name: "maxVelocity").first
+    
     {
-      kinetic: rat_attributes.where(name: "kineticDamage").first.value * multi,
-      thermal: rat_attributes.where(name: "thermalDamage").first.value * multi,
-      explosive: rat_attributes.where(name: "explosiveDamage").first.value * multi,
-      em: rat_attributes.where(name: "emDamage").first.value * multi
+      orbit: rat_attributes.where(name: "entityFlyRange").first.value,
+      cruise: cruise ? cruise.value : 0,
+      max: max ? max.value : 0,
+      range: rat_attributes.where(name: "entityAttackRange").first.value
     }
   end
 end
