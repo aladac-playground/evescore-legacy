d56650fcfac1a731849bcec540e3927c959a79e3
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-07-17 00:16:26 +0200

    removed char info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
-    if @trust == true and  ! session[:char_profile_nag]
-      flash.now[:info] = "Hello #{@char[:name]}, you can visit your profile #{ActionController::Base.helpers.link_to "here", character_profile_path(:char_id => @char[:id])}".html_safe
-      session[:char_profile_nag] = true
-    end
+    # if @trust == true and  ! session[:char_profile_nag]
+    #   flash.now[:info] = "Hello #{@char[:name]}, you can visit your profile #{ActionController::Base.helpers.link_to "here", character_profile_path(:char_id => @char[:id])}".html_safe
+    #   session[:char_profile_nag] = true
+    # end
   end
   def taxes
     
