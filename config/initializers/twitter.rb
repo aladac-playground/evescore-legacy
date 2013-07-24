2deea606720e5b0882b93f05d3b9f65cefc9a8e9
Adam Ladachowski <adam.ladachowski@gmail.com>
2013-07-25 01:07:26 +0200

    added Twitter

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = "T4c69eNubp9IXOlINWDtfQ"
+  config.consumer_secret = "J8tumaECPenPmRkCELKOJca6m9Td9tysOjD2w6tOc"
+  config.oauth_token = "1617595524-ZT1AxudUzICq5haFqjG86nXJPQrFUKWcYbt8VXH"
+  config.oauth_token_secret = "etu6DcI91741tx4KgyV1LY9mhiHDODXLeM2YHuXrZw"
+end
