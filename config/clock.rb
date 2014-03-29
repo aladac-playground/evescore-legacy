feb1d43c92875065cd93f72bab8a5638da7ef5e9
Adam Ladachowski <adam.ladachowski@gmail.com>
2014-03-29 14:20:41 +0100

    pretty print progress

--- config/clock.rb
+++ config/clock.rb
@@ -2,10 +2,20 @@ require_relative "../config/boot"
 require_relative "../config/environment"
  
 class GetWalletData
-  def self.perform
+  def self.perform(rows=3000)
     chars=Char.all
+		count=Char.count
+		i=0
     chars.each do |char|
+			i+=1
+			total=sprintf("%3d",count)
+			current=sprintf("%3d",i)
+			start = Time.now
       char.wallet_import
+			finish = Time.now
+			ts = finish.strftime("%Y-%m-%d %H:%M:%S")
+			duration = sprintf("%.2f",finish-start)
+			puts "#{ts} [#{current}/#{total}] Char: #{char.id}\t Took: #{duration}s"
     end
   end
 end
