1df76bbfc5ff36664baedfd093e8fcd17da69ddf
Adam Ladachowski <adam@saiden.pl>
2014-06-26 13:55:08 +0200

    Tests * keys factory * add api key test

---
+++ spec/factories/keys.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :key do
+    vcode 'TvQTuGXiJJ49Kx6q1Me6LvCmIMOoD7h3T8tWj91jEA7WmYj5IFKPe2pXJP06to4K'
+    id '3480440'
+  end
+end
